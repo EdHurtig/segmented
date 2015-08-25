ffffa45f6d061937d4756e47c0cb08df0718bc45
Eddie Hurtig <EdHurtig@users.noreply.github.com>
2015-08-24 21:26:19 -0400

    Clean up comments and add Big O notation comments

--- segmented.py
+++ segmented.py
@@ -1,19 +1,6 @@
-#include <cstdio>
-#include <algorithm>
-#include <stack>
-#include <limits>
-#define MAXN 1000
-#define INF numeric_limits<double>::infinity()
-# using namespace std;
-#
-# struct Point {
-# 	int x, y;
-# 	bool operator < (const Point& that) const {
-# 		return x < that.x;
-# 	}
-# } points[MAXN + 1];
 import numpy
 
+# Describes a 2D point with an X and a Y coordinate
 class Point:
 	""" Point class represents and manipulates x,y coords. """
 
@@ -43,37 +30,8 @@ class Point:
 	def __str__(self):
 		return "x: %f, y: %f" % (self.x, self.y)
 
-
+# Takes a list of Point Objects (Ordered by X in ASC order) and a Constant C
 def segmented(points,C):
-# // Used for computing E[i][j] which is the square error of a segment
-# // that is best fit to the points {points[i], points[i+1], ..., points[j]}
-#
-# // cumulative_x[i] is sum(points[j].x) for 1 <= j <= i
-# // cumulative_y[i] is sum(points[j].y) for 1 <= j <= i
-# // cumulative_xy[i] is sum(points[j].x * points[j].y) for 1 <= j <= i
-# // cumulative_xSqr[i] is sum(points[j].x * points[j].x) for 1 <= j <= i
-#
-# // slope[i][j] is the slope of the segment that is best fit to
-# // the points {points[i], points[i+1], ..., points[j]}
-#
-# // intercept[i][j] is the y-intercept of the segment that is best fit to
-# // the points {points[i], points[i+1], ..., points[j]}
-#
-# // E[i][j] is the square error of the segment that is best fit to
-# // the points {points[i], points[i+1], ..., points[j]}
-
-# long long cumulative_x[MAXN + 1], cumulative_y[MAXN + 1], cumulative_xy[MAXN + 1], cumulative_xSqr[MAXN + 1];
-# double slope[MAXN + 1][MAXN + 1], intercept[MAXN + 1][MAXN + 1], E[MAXN + 1][MAXN + 1];
-
-
-
-
-# // OPT[i] is the optimal solution (minimum cost) for the points {points[1], points[2], ..., points[i]}
-# double OPT[MAXN + 1];
-
-# // [opt_segment[i], i] is the last segment in the optimal solution
-# // for the points {points[1], points[2], ..., points[i]}
-#int opt_segment[MAXN + 1];
 	N = i = j = k = interval = 0
 	x_sum = 0
 	y_sum = 0
@@ -88,27 +46,25 @@ def segmented(points,C):
 
 	print "Got %d points" % N
 
-	# sort the points in non-decreasing order of x coordinate
-	# sort (points + 1, points + N + 1);
-
 	# Multi dimentional array that is N*N in size
 	slope = numpy.zeros((N+1, N+1))
 	intercept = numpy.zeros((N+1, N+1))
 	E = numpy.zeros((N+1, N+1))
 
 	# precompute the error terms
-
 	cumulative_x = [None]*(N+1)
 	cumulative_y = [None]*(N+1)
 	cumulative_xy = [None]*(N+1)
 	cumulative_xSqr = [None]*(N+1)
 	cumulative_x[0] = cumulative_y[0] = cumulative_xy[0] = cumulative_xSqr[0] = 0
+	
+	# O(n^3)
 	for j in range(1,N+1):
 		cumulative_x[j] = cumulative_x[j-1] + points[j-1].x;
 		cumulative_y[j] = cumulative_y[j-1] + points[j-1].y;
 		cumulative_xy[j] = cumulative_xy[j-1] + points[j-1].x * points[j-1].y;
 		cumulative_xSqr[j] = cumulative_xSqr[j-1] + points[j-1].x * points[j-1].x;
-
+		# O(n^2)
 		for i in range(1,j+1):
 			interval = j - i + 1
 			x_sum = cumulative_x[j] - cumulative_x[i-1]
@@ -127,17 +83,21 @@ def segmented(points,C):
 			intercept[i][j] = (y_sum - slope[i][j] * x_sum) / interval
 
 			E[i][j] = 0.0
+			
+			# O(n)
 			for k in range(i,j+1):
 				tmp = points[k-1].y - slope[i][j] * points[k-1].x - intercept[i][j]
 				E[i][j] += tmp * tmp;
 
 
 
-	# // find the cost of the optimal solution
+	# find the cost of the optimal solution
 	OPT = [None]*(N+1)
 	opt_segment = [None]*(N+1)
 	OPT[0] = 0
 	opt_segment[0] = 0;
+	
+	# O(n)
 	for j in range(1, N+1):
 		mn = INF
 		k = 0
@@ -153,10 +113,11 @@ def segmented(points,C):
 
 	print "Cost of the optimal solution : %lf" % OPT[N]
 
-	# // find the optimal solution
+	# find the optimal solution
 	segments = []
 	i = N
 	j = opt_segment[N]
+	# O(n)
 	while (i > 0):
 		segments.append(i)
 		segments.append(j)
@@ -165,6 +126,7 @@ def segmented(points,C):
 
 	print "\nAn optimal solution :"
 	returns = []
+	# O(n)
 	while (segments != []):
 		i = segments.pop()
 		j = segments.pop()
